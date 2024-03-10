a47096045ab4704dcfc067f7ef9840658deacd98
developerMubarok <developermubarok@gmail.com>
2024-03-10 23:39:51 +0600

    grid template added

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,10 +1,14 @@
 import './Country.css';
 const Counttry = ({country}) => {
-    const {name, flags}= country;
+    const {name, flags, population, area, 
+    }= country;
     return (
         <div className='country'>
             <h3>Name:{name?.common} </h3>
             <img src={flags.png} alt="" />
+            <p>Population: {population} </p>
+            <p>Area: {area} </p>
+            
         </div>
     );
 };
